492f5fc58ad1334a3cff3d9818c857492dd7c840
Abin Varghese <abin.varghese@mitsogo.com>
2022-10-19 19:37:29 +0530

    refactor: remove commented code (test)

--- app/components/product/details.js
+++ app/components/product/details.js
@@ -1,7 +1,6 @@
 import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
 import { action } from '@ember/object';
-// import { tracked } from '@glimmer/tracking';
 
 class CartComponent extends Component {
   @service cart;
