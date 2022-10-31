d68c00c8aab69a8f7ae58d83bab8f8d028062486
Abin Varghese <abin.varghese@mitsogo.com>
2022-10-31 12:57:59 +0530

    refactor: controllers to components

---
+++ app/components/product/info.js
@@ -0,0 +1,12 @@
+import Component from '@glimmer/component';
+
+export default class ProductInfoComponent extends Component {
+
+    dummy = {
+        imgSrc : '',
+        title:'titleee',
+        currentPrice:10,
+        originalPrice:8
+        }
+
+}
